69b920d1f60de338d428b693ac9c604b2269f8ed
dzinoviev <danzino21@gmail.com>
2020-11-26 21:47:51 +0200

    Add JSDoc

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ document.addEventListener("DOMContentLoaded", function () {
   new Filter("#filter--displacementMap", "#feDisplacementMap", "scale");
 });
 
+/**
+ * @description
+ * The function binds input value with SVG filter property.
+ * So, if user will update an input SVG updates automatically.
+ * 
+ * @param {string} inputSelector
+ * @param {string} targetSelector
+ * @param {string} property
+ * @returns {void}
+ */
 function Filter(inputSelector, targetSelector, property) {
   var input = document.querySelector(inputSelector);
   var target = document.querySelector(targetSelector);
